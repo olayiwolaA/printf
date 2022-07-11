a81833fdc0757b4c9094d83b6e8dacac04371ea9
olayiwolaA <mike.ow994@gmail.com>
2022-07-11 03:42:09 -0700

    Files Modified

--- long_dex_print.c
+++ long_dex_print.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * prinlupx - prints a long decimal in hexadecimal
+ * @arguments: The character to print
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
 int prinlupx(va_list arguments, char *buf, unsigned int ibuf)
 {
 	long int int_input, i, isnegative, count, first_digit;
